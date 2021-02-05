33c9ac9c32e728f182d9281b780d6aeebe3de60b
mahzoun <mohammad.mahzoun@gmail.com>
2021-02-05 13:45:20 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/SPUF2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPUF2.dir/main.cpp.o"
+  "SPUF2"
+  "SPUF2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPUF2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
